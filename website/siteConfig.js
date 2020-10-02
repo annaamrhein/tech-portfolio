24fc83bfac849f484491bfa4810bb88ab15393dd
annaamrhein <42839250+annaamrhein@users.noreply.github.com>
2020-10-02 14:30:00 -0500

    profile pic

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -14,7 +14,8 @@ const users = [];
 const siteConfig = {
     title: 'Anna Amrhein', // Title for your website.
     tagline: 'Continuously improving, aspiring technologist.',
-    profilePhoto: 'img/anna.png', 
+    //profilePhoto: 'img/profile.png', 
+    profilePhoto: 'img/anna.JPG',
     url: 'https://annaamrhein.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
